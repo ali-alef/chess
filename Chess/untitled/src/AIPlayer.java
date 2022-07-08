d1c1d1f3ce538c47eea2baa0bc822626a62372ba
ali alef <90463287+ali-alef@users.noreply.github.com>
2022-07-08 21:11:46 +0430

    Delete Chess directory

--- Chess/untitled/src/AIPlayer.java
+++
@@ -1,8 +0,0 @@
-public class AIPlayer implements Player {
-
-    @Override
-    public int[][] nextMove(int[][] chess, boolean white) {
-        MiniMax.MiniMaxState miniMaxState = MiniMax.miniMax(chess, white, 4);
-        return miniMaxState.getChess();
-    }
-}
--- Chess/untitled/src/Player.java
+++
@@ -1,4 +0,0 @@
-public interface Player {
-
-    abstract int[][] nextMove(int[][] positions, boolean white);
-}
